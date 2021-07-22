7058b9e2bde6ac11c1147b6ed1e35741d1454590
julianperna <70858276+julianperna@users.noreply.github.com>
2021-07-22 20:02:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const inpSearch = document.getElementById('inp-search');
+const salida = document.getElementById('salida');
+
+window.addEventListener('load', () => {
+    loader();
+    fetchCharcters();
+});
+
+inpSearch.addEventListener('change', () => {
+    let searchQuery = inpSearch.value;
+    loader();
+    fetchCharcters(searchQuery);
+});
+
+function loader(){
+    salida.innerHTML = '<div class="gif-spinner mx-auto"><img src="https://i.pinimg.com/originals/90/80/60/9080607321ab98fa3e70dd24b2513a20.gif"></img></div>'
+}
+
+async function fetchCharcters(a){
+    let res;
+
+    if(a){
+        res = await fetch(`https://www.breakingbadapi.com/api/characters?name=${a}`);
+    }else{
+        res = await fetch('https://www.breakingbadapi.com/api/characters');
+    }
+
+    let results = await res.json();
+
+    salida.innerHTML = "";
+
+    results.map(result => {
+        const htmlString = `<img src="${result.img}" class="img">
+            <div class="info-display">
+                <h5>Nombre Actor: ${result.portrayed}</h5>
+                <hr>
+                <h6>Nombre: <span>${result.name}</span></h6>
+                <h6>Apodo: <span>${result.nickname}</span></h6>
+                <h6>Serie: <span>${result.category}</span></h6>
+                <h6>Estado: <span>${result.status === "Alive" ? "Vivo" : "Muerto"
+            }</span></h6>
+            </div>`;
+    
+    let salidaString = document.createElement('div');
+    salidaString.classList.add('col-md-3', 'mb-3', 'img-info');
+    salidaString.innerHTML = htmlString;
+    salida.appendChild(salidaString);
+    });
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+#  Breaking Bad
+
+
+_Creacion de Pagina Web utilizando una API_
+
+### Sitio Web Responsive
+
+
+En este repositorio encontraras la API de Breaking Bad utilizando Vanilla JS completamente responsive.
